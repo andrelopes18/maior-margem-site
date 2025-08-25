b3dbb6bd5dcddec36c51020201a85a3ac4d4202f
andrelopes18 <64736157+andrelopes18@users.noreply.github.com>
2025-08-24 21:16:28 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
-
 // Arquivo único de configuração do site
 window.CONFIG = {
   SUPABASE_URL: "https://maprezivxfitqyifmepq.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hcHJleml2eGZpdHF5aWZtZXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MTc4OTcsImV4cCI6MjA2OTE5Mzg5N30.qz8iMEZY0mkh5QgeBKh8lYLITRjcAADbApJkGFwACJI",
-  SITE_URL: "https://maior-margem-site.vercel.app",  // usado no signUp e reset
-  HOME_URL: "https://maior-margem-site.vercel.app"   // usado no reset.html
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hcHJleml2eGZpdHF5cWlmbWVwcSIsInJvbGUiOiJhb25vIiwiaWF0IjoxNzUzNjE3ODk3LCJleHAiOjIwNjkxOTM4OTd9.qz8iMEZY0mkh5QgeBKh8lYLITRjcAADbApJkGFwACJI",
+
+  // usado no signUp e reset
+  SITE_URL: "https://maior-margem-site.vercel.app",  
+
+  // usado no reset.html
+  HOME_URL: "https://maior-margem-site.vercel.app",  
+
+  // base das suas edge functions
+  FUNCTIONS_BASE: "https://maprezivxfitqyifmepq.functions.supabase.co"
 };
