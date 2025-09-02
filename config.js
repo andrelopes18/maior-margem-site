8e94858acb398bd168cc384db9d727e9d37204dd
andrelopes18 <64736157+andrelopes18@users.noreply.github.com>
2025-09-02 20:05:13 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-
 // Arquivo único de configuração do site
 window.CONFIG = {
   SUPABASE_URL: "https://maprezivxfitqyifmepq.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hcHJleml2eGZpdHF5aWZtZXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MTc4OTcsImV4cCI6MjA2OTE5Mzg5N30.qz8iMEZY0mkh5QgeBKh8lYLITRjcAADbApJkGFwACJI",
+
   SITE_URL: "https://maior-margem-site.vercel.app",  // usado no signUp e reset
-  HOME_URL: "https://maior-margem-site.vercel.app"   // usado no reset.html
-  
-    MP_LINKS: {
-    basico: "hhttps://mpago.li/1fdzL4B",
+  HOME_URL: "https://maior-margem-site.vercel.app",  // usado no reset.html
+
+  MP_LINKS: {
+    basico: "https://mpago.li/1fdzL4B",
     intermediario: "https://mpago.li/1fdzL4B",
     premium: "https://mpago.li/1fdzL4B"
+  }
 };
