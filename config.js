7ee291c217d642bb384403f9b39114084937df37
andrelopes18 <64736157+andrelopes18@users.noreply.github.com>
2025-09-06 16:39:58 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.CONFIG = {
   // === Supabase
   SUPABASE_URL: "https://maprezivxfitqyifmepq.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hcHJleml2eGZpdHF5aWZtZXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MTc4OTcsImV4cCI6MjA2OTE5Mzg5N30.qz8iMEZY0mkh5QgeBKh8lYLITRjcAADbApJkGFwACJI",
-
+  FUNCTIONS_URL: "https://maprezivxfitqyifmepq.functions.supabase.co",
   // (REMOVIDO) SERVICE_ROLE_KEY
 
   // === Mercado Pago (TESTE)
